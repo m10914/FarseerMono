2e983dd6fbfb25370c46640b2b7324ae8a446df3
Damon Wall <dw10914@gmail.com>
2013-09-10 09:42:54 +0700

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+	using System.Diagnostics;
+
+	using FarseerMono;
+	using FarseerMono.Dynamics;
+	using FarseerMono.Factories;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var world = new World(new Vector2(-5, -5));
+
+			/*for (int i = 0; i < 1000; i++)
+			{
+				BodyFactory.CreateRectangle(world, 10, 10, 0.1f, new Vector2(i * 10, 0), null);
+			}*/
+			Body bdy = BodyFactory.CreateRectangle(world, 10, 10, 0.5f, Vector2.One, null);
+			bdy.IsStatic = false;
+			bdy.BodyType = BodyType.Dynamic;
+			bdy.Restitution = 0.3f;
+			bdy.Friction = 0.3f;
+
+			Stopwatch timer = new Stopwatch();
+			timer.Start();
+
+			for (int j = 0; j < 1000; j++)
+			{
+				world.Step(16f);
+				Console.WriteLine("Pos: " + bdy.Position.Y);
+			}
+				
+
+			Console.WriteLine("Total time: " + timer.ElapsedMilliseconds);
+
+			Console.ReadKey();
+		}
+	}
+}
